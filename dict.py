361d4ae13ddd450cdc153d7b31eb6b652144097d
OriLight <61224409+OriLight152@users.noreply.github.com>
2022-12-24 09:57:55 +0800

    Initial commit

---
+++ dict.py
@@ -0,0 +1,175 @@
+import re
+import time
+import sys, csv
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+from ui.ui_main_window import Ui_MainWindow
+
+dict_all = []
+dict_index = {}
+search_text = ''
+search_result = []
+
+
+class MainWindowSignal(QObject):
+    update_loadprogress = Signal(str)
+    finish_loadprogress = Signal()
+    finish_search = Signal()
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowIcon(QIcon(f'./icon.png'))
+        self.sig = MainWindowSignal()
+        self.sig.update_loadprogress.connect(self.handle_update_loadprogress)
+        self.sig.finish_loadprogress.connect(self.handle_finish_loadprogress)
+        self.sig.finish_search.connect(self.refresh_list)
+        self.ui.btn_search.clicked.connect(self.handle_search)
+        self.ui.edit_search.textChanged.connect(self.handle_search)
+        self.ui.list_search.currentItemChanged.connect(self.handle_click_item)
+        self.ui.edit_search.setValidator(QRegularExpressionValidator('^[a-z]{0,30}$'))
+        self.ui.btn_search.setDisabled(True)
+        self.ui.edit_search.setDisabled(True)
+
+        self.load_data()
+
+    def load_data(self):
+        self.th = LoadDataThread(self.sig.update_loadprogress)
+        self.th.start()
+        self.th.finished.connect(self.sig.finish_loadprogress)
+
+    def handle_update_loadprogress(self, text: str):
+        self.ui.label_status.setText(text)
+
+    def handle_finish_loadprogress(self):
+        global dict_all, dict_index
+        self.ui.label_status.setText(f'词典加载完毕，共 {len(dict_all)} 条数据')
+        dict_index = dict(
+            zip([x['word'].lower().replace('-', '').replace(' ', '') for x in dict_all], range(len(dict_all))))
+        self.ui.btn_search.setDisabled(False)
+        self.ui.edit_search.setDisabled(False)
+
+    def handle_search(self):
+        global search_text
+
+        print('handle search')
+        # if (self.ui.edit_search.text() == search_text):
+        #     return
+        search_text = self.ui.edit_search.text()
+        if (hasattr(self, 'st')):
+            if (self.st.isRunning()):
+                self.st.terminate()
+        self.st = SearchThread(self.sig.finish_search, self.ui.option_enablefuzzysearch.isChecked())
+        self.st.start()
+
+    def handle_click_item(self):
+        try:
+            data = dict_all[search_result[self.ui.list_search.currentIndex().row()]]
+            self.ui.label_word.setText(data['word'])
+            self.ui.label_translation.setText(data['translation'].replace('\\n', '\n').replace('\\r', ''))
+        except:
+            pass
+
+    def refresh_list(self):
+        self.ui.list_search.clear()
+        for item in search_result:
+            data = dict_all[item]
+            self.ui.list_search.addItem(data['word'] + ' ' + data['translation'].replace('\\n', ' '))
+
+
+class SearchThread(QThread):
+
+    def __init__(self, signal, fuzzy) -> None:
+        super().__init__()
+        self.signal = signal
+        self.fuzzy = fuzzy
+
+    def run(self):
+        global search_result
+
+        # 优化连续输入相应
+        time.sleep(0.3)
+        if (search_text == ''):
+            return
+        result = []
+        pattern_front = search_text + '(.*?)'
+        pattern_fuzzy = '(.*?)' + '(.*?)'.join(list(search_text)) + '(.*?)'
+
+        # 精确匹配
+        if (dict_index.get(search_text)):
+            result.append(dict_index.get(search_text))
+
+        # 前缀匹配
+        for item in dict_index.keys():
+            if re.match(pattern_front, item):
+                if (result.count(dict_index.get(item)) > 0):
+                    continue
+                result.append(dict_index.get(item))
+                if (len(result) >= 40):
+                    search_result = result
+                    self.signal.emit()
+                    return
+
+        # 重合匹配
+        for item in dict_index.keys():
+            if item.find(search_text) != -1:
+                if (result.count(dict_index.get(item)) > 0):
+                    continue
+                result.append(dict_index.get(item))
+                if (len(result) >= 40):
+                    search_result = result
+                    self.signal.emit()
+                    return
+
+        if not self.fuzzy:
+            search_result = result
+            self.signal.emit()
+            return
+
+        # 模糊匹配
+        for item in dict_index.keys():
+            if re.match(pattern_fuzzy, item):
+                if (result.count(dict_index.get(item)) > 0):
+                    continue
+                result.append(dict_index.get(item))
+                if (len(result) >= 40):
+                    search_result = result
+                    self.signal.emit()
+                    return
+
+        search_result = result
+        self.signal.emit()
+
+
+class LoadDataThread(QThread):
+
+    def __init__(self, signal) -> None:
+        super().__init__()
+        self.signal = signal
+
+    def run(self) -> None:
+        time_start = time.time()
+        with open('dict.csv', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            count = 0
+            for item in reader:
+                dict_all.append({'word': item[0], 'translation': item[3]})
+                count += 1
+                if (count >= 1024):
+                    self.signal.emit(f'词典加载中，已加载 {len(dict_all)} 条数据')
+                    count = 0
+        time_end = time.time()
+        print(f'载入耗时：{time_end-time_start}秒')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
---
+++ ui/ui_main_window.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QMainWindow, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(700, 312)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QSize(700, 312))
+        MainWindow.setMaximumSize(QSize(700, 312))
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label_status = QLabel(self.centralwidget)
+        self.label_status.setObjectName(u"label_status")
+        self.label_status.setGeometry(QRect(10, 10, 381, 21))
+        self.edit_search = QLineEdit(self.centralwidget)
+        self.edit_search.setObjectName(u"edit_search")
+        self.edit_search.setGeometry(QRect(10, 40, 351, 31))
+        self.label_translation = QLabel(self.centralwidget)
+        self.label_translation.setObjectName(u"label_translation")
+        self.label_translation.setGeometry(QRect(400, 80, 290, 220))
+        self.label_translation.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.label_translation.setWordWrap(True)
+        self.list_search = QListWidget(self.centralwidget)
+        self.list_search.setObjectName(u"list_search")
+        self.list_search.setGeometry(QRect(10, 70, 381, 231))
+        self.label_word = QLabel(self.centralwidget)
+        self.label_word.setObjectName(u"label_word")
+        self.label_word.setGeometry(QRect(400, 40, 291, 31))
+        font = QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        self.label_word.setFont(font)
+        self.option_enablefuzzysearch = QCheckBox(self.centralwidget)
+        self.option_enablefuzzysearch.setObjectName(u"option_enablefuzzysearch")
+        self.option_enablefuzzysearch.setGeometry(QRect(400, 10, 291, 21))
+        self.btn_search = QPushButton(self.centralwidget)
+        self.btn_search.setObjectName(u"btn_search")
+        self.btn_search.setGeometry(QRect(360, 40, 31, 31))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u82f1\u6c49\u5c0f\u8bcd\u5178", None))
+        self.label_status.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_translation.setText(QCoreApplication.translate("MainWindow", u"\u7ffb\u8bd1", None))
+        self.label_word.setText(QCoreApplication.translate("MainWindow", u"\u5355\u8bcd", None))
+#if QT_CONFIG(tooltip)
+        self.option_enablefuzzysearch.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+        self.option_enablefuzzysearch.setText(QCoreApplication.translate("MainWindow", u"\u6a21\u7cca\u641c\u7d22\uff08\u53ef\u80fd\u4f1a\u589e\u52a0\u641c\u7d22\u7528\u65f6\u6216\u5bfc\u81f4\u7a0b\u5e8f\u5d29\u6e83\uff09", None))
+        self.btn_search.setText(QCoreApplication.translate("MainWindow", u"\u641c", None))
+    # retranslateUi
+
